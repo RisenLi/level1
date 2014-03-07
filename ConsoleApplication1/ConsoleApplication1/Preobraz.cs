d9357657fa567e00407b7ae250b2702fe63ce36a
Anatoliy Lapin <mir.risen-li@mail.ru>
2014-03-07 17:17:37 +0600

    Первая лабораторная
    выполнено

---
+++ ConsoleApplication1/ConsoleApplication1/Preobraz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    static class Preobraz
+    {
+        public static string UdalSimv(string word)
+        {
+            string result = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!(" —–-.,:;!&?[]{}<>%$()'\"".Contains(word[i]) || word[i] == '\n' || word[i] == '\r'))
+                    result += word[i];
+            }
+            return result;
+        }
+        public static List<string> Unicul(List<string> starSpisok)
+        {
+            List<string> result = new List<string>();
+            foreach (var word in starSpisok)
+                if (!result.Contains(word.ToLower()))
+                    result.Add(word.ToLower());
+            return result;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            int count = 0;
+            List<String> text = new List<string>();
+            text.AddRange(Clipboard.GetText().Split(' ', ',', '.', ':', ';', '\n', '\r', '?', '!', '–', '-', '/', '—', '='));
+            text.Sort();
+            foreach (var word in Preobraz.Unicul(text))
+                if (word != "")
+                {
+                    if (count == 1000)
+                        break;
+                    Console.WriteLine(Preobraz.UdalSimv(word));
+                    count++;
+                }
+        }
+    }
+}
